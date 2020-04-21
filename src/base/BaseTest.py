b138652238956d7ade03133328faec8bc01cdb0e
jjaros587 <jjaros587@gmail.com>
2020-04-21 18:35:45 +0200

    change names of keywords for test structuring

--- src/base/BaseTest.py
+++ src/base/BaseTest.py
@@ -10,25 +10,25 @@ class BaseTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.handler = EventHandler()
-        if "set_up_class" in cls.scenario:
-            cls.handler.execute(cls, cls.scenario['set_up_class'])
+        if "setupclass" in cls.scenario:
+            cls.handler.execute(cls, cls.scenario['setup_class'])
 
     def setUp(self):
-        if "set_up" in self.scenario:
-            self.handler.execute(self, self.scenario['set_up'])
+        if "setup" in self.scenario:
+            self.handler.execute(self, self.scenario['setup'])
 
     def base_method(self, **data):
         if "steps" in self.scenario:
             self.handler.execute(self, self.scenario['steps'], data)
 
     def tearDown(self):
-        if "tear_down" in self.scenario:
-            self.handler.execute(self, self.scenario['tear_down'])
+        if "teardown" in self.scenario:
+            self.handler.execute(self, self.scenario['teardown'])
 
     @classmethod
     def tearDownClass(cls):
-        if "tear_down_class" in cls.scenario:
-            cls.handler.execute(cls, cls.scenario['tear_down_class'])
+        if "teardown_class" in cls.scenario:
+            cls.handler.execute(cls, cls.scenario['teardown_class'])
 
 
 def factory(class_name, scenario):
