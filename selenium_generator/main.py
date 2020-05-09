658037d6719507133be4f01a304ad9cb2ebf5652
jjaros587 <jjaros587@gmail.com>
2020-05-09 14:03:43 +0200

    update config handling

--- selenium_generator/main.py
+++ selenium_generator/main.py
@@ -1,13 +1,9 @@
 from selenium_generator.test_runner import loader
 from selenium_generator.test_runner.runner import Runner
 from selenium_generator.parsers.config_parser import ConfigParser
-from selenium_generator.parsers.arg_parser import ArgParser
 
 
 def main():
-    config_path = ArgParser.parse_args()
-    ConfigParser().load_config(config_path)
-
     runner = Runner(**ConfigParser().get_report_params())
     runner.run(loader.Loader().load_scenarios())
 
--- selenium_generator/parsers/arg_parser.py
+++ selenium_generator/parsers/arg_parser.py
@@ -6,6 +6,6 @@ class ArgParser:
     @classmethod
     def parse_args(cls):
         parse = argparse.ArgumentParser()
-        parse.add_argument("-c", "--config", type=str, required=True, help="Set path to config file")
+        parse.add_argument("-c", "--config", type=str, required=False, help="Set path to config file")
         args = parse.parse_args()
         return args.config
