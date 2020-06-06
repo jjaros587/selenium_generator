f9d14d1c37a16e5bc7990d88e7984ce5c6de0358
jjaros587 <jjaros587@gmail.com>
2020-06-06 11:10:27 +0200

    refactor test factory

--- selenium_generator/factories/tests/test_factory.py
+++ selenium_generator/factories/tests/test_factory.py
@@ -20,35 +20,48 @@ class TestFactory:
         return self
 
     def create(self):
-        try:
-            self.v.validate_scenario(self.scenario)
-        except InvalidScenario as e:
-            setattr(self.test_class, "errors", str(self.v.get_errors()))
-            self._create_test_method()
+        if not self._validate_scenario():
             return self.test_class
 
-        if "skip" in self.scenario:
-            self._create_test_method(
-                unittest.skip(self.scenario['skip'])(getattr(self.test_class, self.test_method.__name__))
-            )
+        if self._check_skip():
             return self.test_class
 
-        self._check_data()
-        return ddt.ddt(self.test_class)
+        if self._check_data():
+            return ddt.ddt(self.test_class)
+        return self.test_class
 
     def _check_data(self):
         if 'data' not in self.scenario or self.scenario['data'] is None:
             self._create_test_method()
-            return
+            return False
 
         if isinstance(self.scenario['data'], str):
             data_path = ConfigParser().get_data_path(self.scenario['data'])
             self._create_test_method(
                 ddt.file_data(data_path)(getattr(self.test_class, self.test_method.__name__))
             )
-            return
+            return True
 
         ddt._add_tests_from_data(self.test_class, self._generate_test_name(), self.test_method, self.scenario['data'])
+        return True
+
+    def _validate_scenario(self):
+        try:
+            self.v.validate_scenario(self.scenario)
+        except InvalidScenario as e:
+            setattr(self.test_class, "errors", str(self.v.get_errors()))
+            self._create_test_method()
+            return False
+        else:
+            return True
+
+    def _check_skip(self):
+        if "skip" in self.scenario:
+            self._create_test_method(
+                unittest.skip(self.scenario['skip'])(getattr(self.test_class, self.test_method.__name__))
+            )
+            return True
+        return False
 
     def _create_test_method(self, fn=None):
         setattr(self.test_class, self._generate_test_name(), self.test_method if fn is None else fn)
